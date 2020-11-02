8136a187bedf820b1bc7222005b0684a1fa8749a
alf-byrnison <alfonsots09@gmail.com>
2020-11-02 10:08:33 -0300

    actualización de codigo

--- app/src/main/java/cl/alf/conversordv/MainActivity.java
+++ app/src/main/java/cl/alf/conversordv/MainActivity.java
@@ -12,15 +12,31 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String[] datos = new String[] {"USD", "EUR","CLP"};
+    final String[] datos = new String[] {"USD", "EUR","CLP","NZD","JPY","BCV"};
+    //Creacion del array con la denominacion de las monedas
 
     private Spinner monedaActualSP;
     private Spinner monedaCambioSP;
     private EditText valorCambioET;
     private TextView resultadoTV;
+    //Se define la variable de cada control
 
-    final private double factorUsdEur = 0.87;
-    final private double factorClpUsd = 0.54;
+    //se definen valores de factor conversion
+    final private double factorClpEur = 0.0011;
+    final private double factorClpUsd = 0.0013;
+    final private double factorClpNzd = 0.0020;
+    final private double factorClpJpy = 0.14;
+    final private double factorClpBcv = 670.42;
+    final private double factorEurUsd = 1.16;
+    final private double factorEurNzd = 1.76;
+    final private double factorEurJpy = 181.83;
+    final private double factorEurBcv = 603457.76;
+    final private double factorUsdNzd = 1.52;
+    final private double factorUsdJpy = 104.66;
+    final private double factorUsdBcv = 518434.50;
+    final private double factorNzdJpy = 69.05;
+    final private double factorNzdBcv = 342029.38;
+    final private double factorJpyBcv = 4951.90;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,26 +76,110 @@ public class MainActivity extends AppCompatActivity {
 
         double resultadoConversion =0;
         switch (monedaActual){
-            case "USD":
+            case "CLP":
                 if(monedaCambio.equals("EUR")){
-                    resultadoConversion = valorCambio * factorUsdEur;
+                    resultadoConversion = valorCambio * factorClpEur;
                 }
-                if (monedaCambio.equals("CLP")){
+                if (monedaCambio.equals("USD")){
                     resultadoConversion = valorCambio * factorClpUsd;
+                }
+                if(monedaCambio.equals("NZD")){
+                    resultadoConversion = valorCambio * factorClpNzd;
+                }
+                if(monedaCambio.equals("JPY")){
+                    resultadoConversion = valorCambio * factorClpJpy;
+                }
+                if(monedaCambio.equals("BCV")){
+                    resultadoConversion = valorCambio * factorClpBcv;
                 }
                 break;
             case "EUR":
+                if(monedaCambio.equals("CLP")){
+                    resultadoConversion = valorCambio / factorClpEur;
+                }
                 if(monedaCambio.equals("USD")){
-                    resultadoConversion = valorCambio / factorUsdEur;
+                    resultadoConversion = valorCambio * factorEurUsd;
+                }
+                if(monedaCambio.equals("NZD")){
+                    resultadoConversion = valorCambio * factorEurNzd;
+                }
+                if(monedaCambio.equals("JPY")){
+                    resultadoConversion = valorCambio * factorEurJpy;
+                }
+                if(monedaCambio.equals("BCV")){
+                    resultadoConversion = valorCambio * factorEurBcv;
                 }
                 break;
-            case "CLP":
-                if(monedaCambio.equals("USD")){
+            case "USD":
+                if(monedaCambio.equals("CLP")){
                     resultadoConversion = valorCambio / factorClpUsd;
+                }
+                if(monedaCambio.equals("EUR")){
+                    resultadoConversion = valorCambio / factorEurUsd;
+                }
+                if(monedaCambio.equals("NZD")){
+                    resultadoConversion = valorCambio * factorUsdNzd;
+                }
+                if(monedaCambio.equals("JPY")){
+                    resultadoConversion = valorCambio * factorUsdJpy;
+                }
+                if(monedaCambio.equals("BCV")){
+                    resultadoConversion = valorCambio * factorUsdBcv;
+                }
+                break;
+            case "NZD":
+                if(monedaCambio.equals("CLP")){
+                    resultadoConversion = valorCambio / factorClpNzd;
+                }
+                if(monedaCambio.equals("EUR")){
+                    resultadoConversion = valorCambio / factorEurNzd;
+                }
+                if(monedaCambio.equals("USD")){
+                    resultadoConversion = valorCambio / factorUsdNzd;
+                }
+                if(monedaCambio.equals("JPY")){
+                    resultadoConversion = valorCambio * factorNzdJpy;
+                }
+                if(monedaCambio.equals("BCV")){
+                    resultadoConversion = valorCambio * factorNzdBcv;
+                }
+                break;
+            case "JPY":
+                if(monedaCambio.equals("CLP")){
+                    resultadoConversion = valorCambio / factorClpJpy;
+                }
+                if(monedaCambio.equals("EUR")){
+                    resultadoConversion = valorCambio / factorEurJpy;
+                }
+                if(monedaCambio.equals("USD")){
+                    resultadoConversion = valorCambio / factorUsdJpy;
+                }
+                if(monedaCambio.equals("NZD")){
+                    resultadoConversion = valorCambio / factorNzdJpy;
+                }
+                if(monedaCambio.equals("BCV")){
+                    resultadoConversion = valorCambio * factorJpyBcv;
+                }
+                break;
+            case "BCV":
+                if(monedaCambio.equals("CLP")){
+                    resultadoConversion = valorCambio / factorClpBcv;
+                }
+                if(monedaCambio.equals("EUR")){
+                    resultadoConversion = valorCambio / factorEurBcv;
+                }
+                if(monedaCambio.equals("USD")){
+                    resultadoConversion = valorCambio / factorUsdBcv;
+                }
+                if(monedaCambio.equals("NZD")){
+                    resultadoConversion = valorCambio / factorNzdBcv;
+                }
+                if(monedaCambio.equals("JPY")){
+                    resultadoConversion = valorCambio / factorJpyBcv;
                 }
                 break;
         }
 
-        return 0;
+        return resultadoConversion;
     }
 }
